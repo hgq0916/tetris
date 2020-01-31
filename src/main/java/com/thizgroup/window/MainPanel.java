4b8e97f4afd1816cfd48dd80d5d0cbfac6f23033
hgq0916 <1286908118@qq.com>
2020-01-31 16:51:50 +0800

    处理游戏结束

--- src/main/java/com/thizgroup/window/MainPanel.java
+++ src/main/java/com/thizgroup/window/MainPanel.java
@@ -49,7 +49,7 @@ public class MainPanel extends JPanel {
   public MainPanel(MainWindow mainWindow){
 
     this.mainWindow = mainWindow;
-    this.setBackground(Color.WHITE);
+    this.setBackground(Color.BLACK);
     this.setBounds(25,44,PANEL_WIDTH,PANEL_HEIGHT);
     //生成图片
     try {
@@ -92,7 +92,7 @@ public class MainPanel extends JPanel {
   public void paint(Graphics g) {
     super.paint(g);
     //画线
-    drawLine(g);
+    //drawLine(g);
     //画方块
     drawBlocks(g);
     if(gameOver){
